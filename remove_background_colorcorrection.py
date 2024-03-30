6017a9d4fc23dfdc9957fef8a9088aa1354fce99
Petr Kulikov <peterkulikoff@yandex.ru>
2024-03-30 13:49:02 +0300

    correct size

--- remove_background_colorcorrection.py
+++ remove_background_colorcorrection.py
@@ -1,41 +1,47 @@
 import os
 from rembg import remove
-from PIL import Image, ImageEnhance
+from PIL import Image
 
 desired_size = (500, 500)
+MAX_SIZE = 450
 USER_INPUT = "/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото запчасти"
 USER_OUTPUT = '/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото обработанные'
 
-def remove_bg(input_path, output_path):
+def process_image(input_path, output_path):
     for pict in os.listdir(input_path):
-        if pict.endswith('.png') or pict.endswith('.JPG') or pict.endswith('.jpeg'):
-            print(f'[+] Удаляю фон: "{pict}"...')
+        if pict.endswith('.png') or pict.endswith('.JPG') or pict.endswith('.CR2'):
+            print(f'[+] Обрабатываю изображение: "{pict}"...')
+            # Открытие изображения
+            image = Image.open(os.path.join(input_path, pict))
+
             # Удаление фона
-            output = remove(Image.open(os.path.join(input_path, pict)))
+            output = remove(image)
 
-            # Увеличение контрастности
-            enhancer = ImageEnhance.Contrast(output)
-            output = enhancer.enhance(1.2)  # Увеличиваем контрастность в 1.2 раза
+            # Создание белого фона с альфа-каналом
+            white_bg = Image.new("RGBA", desired_size, (255, 255, 255, 255))
 
-            # Увеличение резкости
-            enhancer = ImageEnhance.Sharpness(output)
-            output = enhancer.enhance(1.5)  # Увеличиваем резкость в 1.5 раза
+            # Вырезание изображения с фона
+            bbox = output.getbbox()
+            output = output.crop(bbox)
 
-            # Увеличение яркости
-            enhancer = ImageEnhance.Brightness(output)
-            output = enhancer.enhance(1.2)  # Увеличиваем яркость в 1.2 раза
+            # Уменьшение размера изображения с сохранением пропорций
+            width, height = output.size
+            max_dimension = max(width, height)
+            if max_dimension > MAX_SIZE:
+                scale = MAX_SIZE / max_dimension
+                new_width = int(width * scale)
+                new_height = int(height * scale)
+                output = output.resize((new_width, new_height), Image.LANCZOS)
 
-            # Увеличение разрешения
-            output = output.resize(desired_size, Image.LANCZOS)
+            # Вставка вырезанного изображения на белый фон
+            x_offset = (desired_size[0] - output.size[0]) // 2
+            y_offset = (desired_size[1] - output.size[1]) // 2
+            white_bg.paste(output, (x_offset, y_offset), output)
 
-            # Создание белого фона
-            white_bg = Image.new("RGB", output.size, (255, 255, 255))
+            # Конвертация изображения в формат RGB
+            white_bg = white_bg.convert("RGB")
 
-            # Установка альфа-канала
-            output_with_alpha = output.convert("RGBA")
-            white_bg.paste(output_with_alpha, (0, 0), output_with_alpha)
-
-            # Сохранение результата
+            # Сохранение результата в формате JPEG
             white_bg.save(os.path.join(output_path, f'{pict.split(".")[0]}.jpg'), 'JPEG')
 
 def get_target_path(user_input):
@@ -46,7 +52,7 @@ def get_target_path(user_input):
 
 def main(user_input, user_output):
     print("Погнали")
-    remove_bg(get_target_path(user_input), user_output)
-    print('\n[+] Удаление фона изображений и добавление белого фона завершено!')
+    process_image(get_target_path(user_input), user_output)
+    print('\n[+] Обработка изображений завершена!')
 
 main(USER_INPUT, USER_OUTPUT)
