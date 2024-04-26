ce3896bc7086a8bcc3c3ffe4d2db46d222177818
Petr Kulikov <peterkulikoff@yandex.ru>
2024-04-26 14:27:57 +0300

    v.2.0.3

--- remove_background_to_web_site 2.0 3.py
+++ remove_background_to_web_site 2.0 3.py
@@ -1,8 +1,10 @@
 import os
 import shutil
 from rembg import remove  # Импортируем функцию для удаления фона
-from PIL import Image, ImageEnhance  # Импортируем модуль для работы с изображениями
-import openpyxl # Импортируем модуль для работы с Excel-файлами
+from PIL import Image, ImageEnhance
+import openpyxl  # Импортируем модуль для работы с Excel-файлами
+from tqdm import tqdm
+
 
 class ImageProcessor:
     def __init__(self, input_path, output_path, excel_file, processed_images_path, original_images_path):
@@ -13,7 +15,7 @@ class ImageProcessor:
         self.processed_images_path = processed_images_path  # Папка для сохранения изображений без фона
         self.original_images_path = original_images_path  # Папка для сохранения исходных изображений
         self.desired_size = (500, 500)  # Размеры изображений
-        self.MAX_SIZE = 450  # Максимальный размер изображения
+        self._MAX_SIZE_MASK = 400  # Максимальный размер изображения
         self.mapping = self.load_mapping()  # Загрузка соответствия имён файлов из Excel
 
     def load_mapping(self):
@@ -28,28 +30,24 @@ class ImageProcessor:
 
     def process_images(self):
         # Обработка изображений
-        print("Начинаю обработку изображений...")
-        print("Словарь имен файлов из Excel:")
-        #for old_filename, new_filename in self.mapping.items():
-         #   print(f"{old_filename} -> {new_filename}")
-
-        total_images = 0  # Общее количество изображений
+        print("\nНачинаю обработку изображений...\n")
+        total_images = sum(
+            1 for file in os.listdir(self.input_path) if self.is_image(os.path.join(self.input_path, file)))
         processed_images = 0  # Количество обработанных изображений
-        for pict in os.listdir(self.input_path):
-            file_path = os.path.join(self.input_path, pict)
-            if os.path.isfile(file_path) and self.is_image(file_path):
-                total_images += 1
-                filename, ext = os.path.splitext(pict)
-                filename = filename  # Убираем последние три символа (расширение и еще один символ)
-                new_filename = self.mapping.get(filename, None)  # Получаем новое имя из словаря
-                print(f"Проверка файла: {filename}, новое имя: {new_filename}")
-                try:
-                    self.process_image(file_path, new_filename)
-                    processed_images += 1
-                except Exception as e:
-                    print(f'Ошибка при обработке изображения {file_path}: {e}')
+        with tqdm(total=total_images, desc="Processing images") as pbar:
+            for file in os.listdir(self.input_path):
+                file_path = os.path.join(self.input_path, file)
+                if os.path.isfile(file_path) and self.is_image(file_path):
+                    filename, ext = os.path.splitext(file)
+                    filename = filename  # Убираем последние три символа (расширение и еще один символ)
+                    new_filename = self.mapping.get(filename, None)  # Получаем новое имя из словаря
 
-        print(f'Обработано изображений: {processed_images} из {total_images}')
+                    if new_filename is not None:
+                        self.process_image(file_path, new_filename)
+                        processed_images += 1
+                        pbar.update(1)
+
+        print("\nОбработка завершена.")
 
     def is_image(self, file_path):
         # Проверка, является ли файл изображением по расширению
@@ -67,17 +65,46 @@ class ImageProcessor:
         # Удаление фона
         output = remove(image)
 
+        # Анализ размера и занятой площади изображения
+        width, height, occupancy = self.analyze_image(output)
+        print("Заполенение = {}".format(occupancy))# Анализ размера и занятой площади изображения
+
+        # Если изображение занимает мало места, увеличиваем его
+        if occupancy < 0.05:
+            self.set_max_sixe(300)
+        else:
+            self.set_max_sixe(450)
+
+        enhancer = ImageEnhance.Sharpness(output)
+        output = enhancer.enhance(20)
+
+        enhancer = ImageEnhance.Color(output)
+        output = enhancer.enhance(1.3)
+
         # Сохранение изображения без фона в формате RGBA
-      
-        
+        if new_filename is not None:
+            processed_image_path = os.path.join(self.processed_images_path, f'{new_filename}.png')
+            output.save(processed_image_path, 'PNG')
+        else:
+            # Если новое имя не найдено в словаре, сохраняем изображение с текущим именем
+            new_filename = os.path.splitext(os.path.basename(file_path))[0]
+            output.save(os.path.join(self.processed_images_path, f'{new_filename}.png'), 'PNG')
+
+        # Получаем bbox и обрезаем изображение до него
+        bbox = output.getbbox()
+        output = output.crop(bbox)
+
         # Уменьшение изображения
         output = self.resize_image(output)
-        
+
         # Вставка изображения на белый фон
         output = self.insert_on_white_background(output)
-        
+
         # Конвертация изображения без фона в формат RGB
         output = output.convert("RGB")
+
+        enhancer = ImageEnhance.Sharpness(output)
+        output = enhancer.enhance(1.5)
 
         if new_filename is not None:
             # Сохранение переименованного изображения в формате JPEG
@@ -87,60 +114,62 @@ class ImageProcessor:
             new_filename = os.path.splitext(os.path.basename(file_path))[0]
             output.save(os.path.join(self.output_path, f'{new_filename}.jpg'), 'JPEG')
 
-        print(f'[+] Обработан и сохранен файл: {file_path}')
+    def set_max_sixe(self, number):
+        self._MAX_SIZE_MASK = number
+
+    def get_max_size(self):
+        return self._MAX_SIZE_MASK
 
     def resize_image(self, image):
         # Изменение размера изображения
         width, height = image.size
         max_dimension = max(width, height)
-        if max_dimension > self.MAX_SIZE:
-            scale = self.MAX_SIZE / max_dimension
-            new_width = int(width * scale)
-            new_height = int(height * scale)
-            return image.resize((new_width, new_height), Image.LANCZOS)
-        elif max_dimension < 350:
-            scale = self.MAX_SIZE / max_dimension
-            print("Увеличиваем размер изображеня {}")
+        if max_dimension > self.get_max_size():
+            scale = self.get_max_size() / max_dimension
             new_width = int(width * scale)
             new_height = int(height * scale)
             return image.resize((new_width, new_height), Image.LANCZOS)
         else:
             return image
 
+    def analyze_image(self, image):
+        # Анализ размера и занятой площади изображения
+        width, height = image.size
+        area = width * height
+        non_transparent_pixels = 0
+        for pixel in image.getdata():
+            if pixel[3] != 0:  # Проверка непрозрачности
+                non_transparent_pixels += 1
+        occupancy = non_transparent_pixels / area
+        return width, height, occupancy
+
     def insert_on_white_background(self, image):
         # Создание белого фона с альфа-каналом в формате RGBA
         white_bg = Image.new("RGBA", self.desired_size, (255, 255, 255, 255))
-    
+
         # Уменьшение изображения
-        image_resized = self.resize_image(image)
-    
+        image_resized = image
+
         # Получаем bbox и обрезаем изображение до него
         bbox = image_resized.getbbox()
         image_resized = image_resized.crop(bbox)
-    
+
         # Вычисляем новые координаты для центрирования обрезанного изображения
         x_offset = (self.desired_size[0] - image_resized.size[0]) // 2
         y_offset = (self.desired_size[1] - image_resized.size[1]) // 2
-    
+
         # Вставка обрезанного изображения на белый фон
         white_bg.paste(image_resized, (x_offset, y_offset), image_resized)
-    
+
         return white_bg
 
 
+# Основная функция программы
+input_path = "/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото запчасти"  # Папка с исходными изображениями
+output_path = '/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото обработанные'  # Папка для сохранения обработанных изображений
+excel_file = '/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото обработанные/Список запчастей для ренейма.xlsx'  # Путь к Excel-файлу с данными
+processed_images_path = "/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото обработанные/Фото запчасти без фона"  # Папка для сохранения изображений без фона
+original_images_path = "/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото обработанные/Фото запчасти исходные"  # Папка для сохранения исходных изображений
 
-
-def main():
-    # Основная функция программы
-    input_path = "/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото запчасти"  # Папка с исходными изображениями
-    output_path = '/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото обработанные'  # Папка для сохранения обработанных изображений
-    excel_file = '/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото обработанные/Список запчастей для ренейма.xlsx'  # Путь к Excel-файлу с данными
-    processed_images_path = "/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото обработанные/Фото запчасти без фона"  # Папка для сохранения изображений без фона
-    original_images_path = "/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото обработанные/Фото запчасти исходные"  # Папка для сохранения исходных изображений
-
-    processor = ImageProcessor(input_path, output_path, excel_file, processed_images_path, original_images_path)
-    processor.process_images()
-
-if __name__ == "__main__":
-    main()
-    
+processor = ImageProcessor(input_path, output_path, excel_file, processed_images_path, original_images_path)
+processor.process_images()
--- remove_background_to_web_site 2.0.py
+++ remove_background_to_web_site 2.0.py
@@ -3,6 +3,9 @@ import shutil
 from rembg import remove  # Импортируем функцию для удаления фона
 from PIL import Image, ImageEnhance, ImageFilter  # Импортируем модуль для работы с изображениями
 import openpyxl # Импортируем модуль для работы с Excel-файлами
+import alive_progress as alive
+import time
+
 
 class ImageProcessor:
     def __init__(self, input_path, output_path, excel_file, processed_images_path, original_images_path):
@@ -13,7 +16,7 @@ class ImageProcessor:
         self.processed_images_path = processed_images_path  # Папка для сохранения изображений без фона
         self.original_images_path = original_images_path  # Папка для сохранения исходных изображений
         self.desired_size = (500, 500)  # Размеры изображений
-        self.MAX_SIZE = 450 # Максимальный размер изображения
+        self.MAX_SIZE = 420 # Максимальный размер изображения
         self.mapping = self.load_mapping()  # Загрузка соответствия имён файлов из Excel
 
     def load_mapping(self):
@@ -32,6 +35,12 @@ class ImageProcessor:
         #print("Словарь имен файлов из Excel:")
         #for old_filename, new_filename in self.mapping.items():
         #   print(f"{old_filename} -> {new_filename}")
+        count_pic = 0
+        for file in os.listdir(self.input_path):
+            file_address = os.path.join(self.input_path, pict)
+            if self.is_image(file_address):
+                count_pic += 1
+
 
         total_images = 0  # Общее количество изображений
         processed_images = 0  # Количество обработанных изображений
@@ -44,7 +53,7 @@ class ImageProcessor:
                 new_filename = self.mapping.get(filename, None)  # Получаем новое имя из словаря
 
                 if new_filename is not None:
-                    print(f"Проверка файла: {filename}, новое имя: {new_filename}")
+                    #print(f"Проверка файла: {filename}, новое имя: {new_filename}")
 
                 try:
                     self.process_image(file_path, new_filename)
@@ -71,7 +80,7 @@ class ImageProcessor:
         output = remove(image)
 
         enhancer = ImageEnhance.Sharpness(output)
-        output = enhancer.enhance(4)
+        output = enhancer.enhance(20)
 
         enhancer = ImageEnhance.Color(output)
         output = enhancer.enhance(1.3)
@@ -89,11 +98,13 @@ class ImageProcessor:
         #output.save(processed_image_path, 'PNG')
 
         # Анализ размера и занятой площади изображения
-        width, height, occupancy = self.analyze_image(output)
+        #width, height, occupancy = self.analyze_image(output)
 
         # Если изображение занимает мало места, увеличиваем его
-        if occupancy < 0.5:
-            output = self.enlarge_image(output)
+        #if occupancy < 0.2:
+         #   self.MAX_SIZE = 370
+        #else:
+        #    self.MAX_SIZE = self.MAX_SIZE_MASK
 
         # Получаем bbox и обрезаем изображение до него
         bbox = output.getbbox()
@@ -107,6 +118,9 @@ class ImageProcessor:
 
         # Конвертация изображения без фона в формат RGB
         output = output.convert("RGB")
+
+        enhancer = ImageEnhance.Sharpness(output)
+        output = enhancer.enhance(1.5)
 
         if new_filename is not None:
             # Сохранение переименованного изображения в формате JPEG
@@ -167,16 +181,14 @@ class ImageProcessor:
 
         return white_bg
 
-def main():
-    # Основная функция программы
-    input_path = "/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото запчасти"  # Папка с исходными изображениями
-    output_path = '/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото обработанные'  # Папка для сохранения обработанных изображений
-    excel_file = '/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото обработанные/Список запчастей для ренейма.xlsx'  # Путь к Excel-файлу с данными
-    processed_images_path = "/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото обработанные/Фото запчасти без фона"  # Папка для сохранения изображений без фона
-    original_images_path = "/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото обработанные/Фото запчасти исходные"  # Папка для сохранения исходных изображений
 
-    processor = ImageProcessor(input_path, output_path, excel_file, processed_images_path, original_images_path)
-    processor.process_images()
+# Основная функция программы
+input_path = "/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото запчасти"  # Папка с исходными изображениями
+output_path = '/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото обработанные'  # Папка для сохранения обработанных изображений
+excel_file = '/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото обработанные/Список запчастей для ренейма.xlsx'  # Путь к Excel-файлу с данными
+processed_images_path = "/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото обработанные/Фото запчасти без фона"  # Папка для сохранения изображений без фона
+original_images_path = "/Volumes/srvfsz/2.4 Сервисная служба (Общая)/Фото обработанные/Фото запчасти исходные"  # Папка для сохранения исходных изображений
 
-if __name__ == "__main__":
-    main()
+processor = ImageProcessor(input_path, output_path, excel_file, processed_images_path, original_images_path)
+processor.process_images()
+
